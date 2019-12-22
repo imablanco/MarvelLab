36986217971f9fea8ddfc9d4b0e8b525a383881a
Alvaro Blanco Cabrero <alvaro.bl91@gmail.com>
2019-12-22 20:05:02 +0100

    Initial commit

---
+++ core/domain/src/main/java/com/ablanco/marvellab/core/domain/model/Character.kt
@@ -0,0 +1,8 @@
+package com.ablanco.marvellab.core.domain.model
+
+/**
+ * Created by Álvaro Blanco Cabrero on 2019-12-22.
+ * MarvelLab.
+ */
+class Character {
+}
---
+++ core/domain/src/main/java/com/ablanco/marvellab/core/domain/repository/CharactersRepository.kt
@@ -0,0 +1,8 @@
+package com.ablanco.marvellab.core.domain.repository
+
+/**
+ * Created by Álvaro Blanco Cabrero on 2019-12-22.
+ * MarvelLab.
+ */
+interface CharactersRepository {
+}
